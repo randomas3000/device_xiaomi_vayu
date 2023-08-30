b7a917850c4e0679160854819722e72457655e5a
vytska69 <vytautas.duchas@gmail.com>
2023-08-30 17:33:45 +0300

    velcome droidx-UI

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_vayu.mk
+    $(LOCAL_DIR)/droidx_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-cherish_vayu-eng \
-cherish_vayu-userdebug \
-cherish_vayu-user
+droidx_vayu-eng \
+droidx_vayu-userdebug \
+droidx_vayu-user
--- droidx_vayu.mk
+++ droidx_vayu.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common CherishOS configurations
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+# Inherit common droidxUI configurations
+$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
 
-PRODUCT_NAME := cherish_vayu
+PRODUCT_NAME := droidx_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
@@ -30,12 +30,12 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.5.0.SJUMIXM:user/release-keys
 
 
-# CherishOS additions
+# droidx additions
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # maintainer stuff
-CHERISH_BUILD_TYPE := OFFICIAL
+DROIDX_BUILD_TYPE := OFFICIAL
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer=randomas3000
+ro.droidx.maintainer= randomas3000
